4daa597e1c06e6fc0a2741622968a41b9580b4e4
Sandesh Katwal <mesandesh.2014@gmail.com>
2024-01-20 22:28:06 +0545

    chapter 16 - adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Next.js Financial Dashboard',
+    default: 'Next.js Financial dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
